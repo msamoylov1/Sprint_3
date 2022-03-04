6ad4c854422670459e459e3fa60b0af73d611f1d
Maxim Samoylov <m.samoylov@timepad.ru>
2022-03-05 02:26:23 +0300

    Тесты - Логин курьера

--- src/test/java/test/LoginCourierTest.java
+++ src/test/java/test/LoginCourierTest.java
@@ -1,37 +1,24 @@
 package test;
 
 import io.qameta.allure.Description;
-import io.qameta.allure.Step;
 import io.qameta.allure.junit4.DisplayName;
-import io.restassured.RestAssured;
-import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.Matchers.equalTo;
+import static test.DataHelper.getRandomString;
 
-public class LoginCourierTest extends DataHalper {
-
-    //генерация рандомного login для курьера
-    String randomLogin = getRandomString(5);
+public class LoginCourierTest extends StepsForTests {
 
     @Before
     public void setUp() {
-        RestAssured.baseURI = "http://qa-scooter.praktikum-services.ru";
-        String json = "{\"login\": \"" + randomLogin + "\", \"password\": \"123\", \"firstName\": \"max1\"}";
-        Response response =
-                given().log().all()
-                        .header("Content-type", "application/json")
-                        .and()
-                        .body(json)
-                        .when()
-                        .post("/api/v1/courier");
-        response.then().log().all().statusCode(201)
-                .and().body("ok", equalTo(true));
+        baseUrl();
+        DataHelper.randomLogin = getRandomString(5);
+        //создаём курьера
+        sendNewCourier();
     }
 
     @Test
@@ -39,174 +26,64 @@ public class LoginCourierTest extends DataHalper {
     @Description("Отправляются все 2 поля - корректно заполненные")
     public void trueLoginCourierTest() {
         //логин за ранее созданного курьера
-        getLoginCourier();
+        Response response = loginCourier();
+        //проверка тела ответа
+        response.then().log().all().statusCode(200)
+                .and().body("id", notNullValue());
     }
 
     @Test
     @DisplayName("Тест ручки /api/v1/courier/login - логин курьера в системе (404 - несуществующий логин)")
     @Description("Отправляются все 2 поля - несуществующий логин, ожидаем ошибку 404")
     public void loginFalseLoginCourierTest() {
-        getFalseLoginCourier();
+        Response response = getFalseLoginCourier();
+        //проверка тела ответа
+        response.then().log().all().statusCode(404)
+                .and().body("message", equalTo("Учетная запись не найдена"));
     }
 
     @Test
     @DisplayName("Тест ручки /api/v1/courier/login - логин курьера в системе (404 - несуществующий пароль)")
     @Description("Отправляются все 2 поля - несуществующий пароль, ожидаем ошибку 404")
     public void loginFalsePassCourierTest() {
-        getFalsePassCourier();
-    }
-
-    @Test
-    @DisplayName("Тест ручки /api/v1/courier/login - логин курьера в системе (400 - пустое/нет поля логин)")
-    @Description("Отправляются 1 поле(password)/2 поля, но в login пусто - ожидаем ошибку 400")
-    public void loginNoLoginCourierTest() {
-        //кейс 1 - пустое поле логин
-        getNullLoginCourier();
-        //кейс 2 - нет поля логин
-        getNoLoginCourier();
-    }
-
-    //1 из тестов не проходит (когда не отправляется поле password - это баг)
-    @Test
-    @DisplayName("Тест ручки /api/v1/courier/login - логин курьера в системе (400 - пустое/нет поля пароль)")
-    @Description("Отправляются 1 поле(login)/2 поля, но в password пусто - ожидаем ошибку 400")
-    public void loginNoPassCourierTest() {
-        //кейс 1 - пустое поле логин
-        getNullPassCourier();
-        //кейс 2 - нет поля логин
-        getNoPassCourier();
-    }
-
-    @Step("Создаём нового курьера")
-    public void sendNewCourier() {
-        String json = "{\"login\": \"" + randomLogin + "\", \"password\": \"123\", \"firstName\": \"max1\"}";
-        Response response =
-                given().log().all()
-                        .header("Content-type", "application/json")
-                        .and()
-                        .body(json)
-                        .when()
-                        .post("/api/v1/courier");
-        response.then().log().all().statusCode(201)
-                .and().body("ok", equalTo(true));
-    }
-
-    @Step("Логинимся за созданного курьера")
-    public void getLoginCourier() {
-        String json = "{\"login\": \""  + randomLogin + "\", \"password\": \"123\"}";
-        Response response =
-                given().log().all()
-                        .header("Content-type", "application/json")
-                        .and()
-                        .body(json)
-                        .when()
-                        .post("/api/v1/courier/login");
-                        response.then().log().all().statusCode(200)
-                        .and().body("id", notNullValue());
-        JsonPath jsonPathOrder = response.jsonPath();
-        idCourier = jsonPathOrder.get("id");
-    }
-
-    @Step("Логинимся за созданного курьера - несуществующий логин")
-    public void getFalseLoginCourier() {
-        String json = "{\"login\": \"t+00+00\", \"password\": \"123\"}";
-        Response response =
-                given().log().all()
-                        .header("Content-type", "application/json")
-                        .and()
-                        .body(json)
-                        .when()
-                        .post("/api/v1/courier/login");
-                        response.then().log().all().statusCode(404)
-                        .and().body("message", equalTo("Учетная запись не найдена"));
-    }
-    @Step("Логинимся за созданного курьера - несуществующий пароль")
-    public void getFalsePassCourier() {
-        String json = "{\"login\": \""  + randomLogin + "\", \"password\": \"0000\"}";
-        Response response =
-                given().log().all()
-                        .header("Content-type", "application/json")
-                        .and()
-                        .body(json)
-                        .when()
-                        .post("/api/v1/courier/login");
+        Response response = getFalsePassCourier();
+        //проверка тела ответа
         response.then().log().all().statusCode(404)
                 .and().body("message", equalTo("Учетная запись не найдена"));
     }
 
-    @Step("Логинимся за созданного курьера - несуществующий логин")
-    public void getNullLoginCourier() {
-        String json = "{\"login\": \"\", \"password\": \"123\"}";
-        Response response =
-                given().log().all()
-                        .header("Content-type", "application/json")
-                        .and()
-                        .body(json)
-                        .when()
-                        .post("/api/v1/courier/login");
-                        response.then().log().all().statusCode(400)
-                        .and().body("message", equalTo("Недостаточно данных для входа"));
-    }
-    @Step("Логинимся за созданного курьера - несуществующий логин")
-    public void getNoLoginCourier() {
-        String json = "{\"password\": \"123\"}";
-        Response response =
-                given().log().all()
-                        .header("Content-type", "application/json")
-                        .and()
-                        .body(json)
-                        .when()
-                        .post("/api/v1/courier/login");
-                        response.then().log().all().statusCode(400)
-                        .and().body("message", equalTo("Недостаточно данных для входа"));
+    @Test
+    @DisplayName("Тест ручки /api/v1/courier/login - логин курьера в системе (400 - пустое/пробел в поле логин)")
+    @Description("Отправляются 1 поле(password)/2 поля, но в login пусто - ожидаем ошибку 400")
+    public void loginNoLoginCourierTest() {
+        //кейс 1 - пустое поле логин
+        Response response = getNullLoginCourier();
+        response.then().log().all().statusCode(400)
+                .and().body("message", equalTo("Недостаточно данных для входа"));
+
+        //кейс 2 - пробел в поле логин
+        getNoLoginCourier();
+        response.then().log().all().statusCode(400)
+                .and().body("message", equalTo("Недостаточно данных для входа"));
     }
 
-
-    @Step("Логинимся за созданного курьера - несуществующий пароль")
-    public void getNullPassCourier() {
-        String json = "{\"login\": \""  + randomLogin + "\", \"password\": \"\"}";
-        Response response =
-                given().log().all()
-                        .header("Content-type", "application/json")
-                        .and()
-                        .body(json)
-                        .when()
-                        .post("/api/v1/courier/login");
-                        response.then().log().all().statusCode(400)
-                        .and().body("message", equalTo("Недостаточно данных для входа"));
-    }
-    @Step("Логинимся за созданного курьера - несуществующий пароль")
-    public void getNoPassCourier() {
-        String json = "{\"login\": \""  + randomLogin + "\"}";
-        Response response =
-                given().log().all()
-                        .header("Content-type", "application/json")
-                        .and()
-                        .body(json)
-                        .when()
-                        .post("/api/v1/courier/login");
-                        response.then().log().all().statusCode(400)
-                        .and().body("message", equalTo("Недостаточно данных для входа"));
+    @Test
+    @DisplayName("Тест ручки /api/v1/courier/login - логин курьера в системе (400 - пустое/пробел в поле пароль)")
+    @Description("Отправляются 1 поле(login)/2 поля, но в password пусто - ожидаем ошибку 400")
+    public void loginNoPassCourierTest() {
+        //пробел в поле пароль
+        Response response = getNoPassCourier();
+        response.then().log().all().statusCode(400)
+                .and().body("message", equalTo("Недостаточно данных для входа"));
     }
 
     @After
     public void deleteCourier() {
-        String json = "{\"login\": \""  + randomLogin + "\", \"password\": \"123\"}";
-        Response response =
-                given().log().all()
-                        .header("Content-type", "application/json")
-                        .and()
-                        .body(json)
-                        .when()
-                        .post("/api/v1/courier/login");
-        JsonPath jsonPathOrder = response.jsonPath();
-        idCourier = jsonPathOrder.get("id");
+        //получаем id курьера
+        loginCourierAndGetId();
+
         //удаление курьера
-        given().log().all()
-                .header("Content-type", "application/json")
-                .and()
-                .when()
-                .delete("/api/v1/courier/" + idCourier);
+        deleteNewCourier();
 
     }
 }
