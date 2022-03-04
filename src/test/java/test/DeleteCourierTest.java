8b50e6e775c31d523d65bce6e104ab50849d7d21
Maxim Samoylov <m.samoylov@timepad.ru>
2022-03-05 02:23:53 +0300

    Тесты - Удаление курьера

--- src/test/java/test/DeleteCourierTest.java
+++ src/test/java/test/DeleteCourierTest.java
@@ -1,39 +1,36 @@
 package test;
 
 import io.qameta.allure.Description;
-import io.qameta.allure.Step;
 import io.qameta.allure.junit4.DisplayName;
-import io.restassured.RestAssured;
-import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
+import static test.DataHelper.getRandomString;
 
-public class DeleteCourierTest extends DataHalper {
-
-    //генерация рандомного login для курьера
-    String randomLogin = getRandomString(5);
+public class DeleteCourierTest extends StepsForTests {
 
     @Before
     public void setUp() {
-        RestAssured.baseURI = "http://qa-scooter.praktikum-services.ru";
+        baseUrl();
+        DataHelper.randomLogin = getRandomString(5);
+        //создаём курьера
+        sendNewCourier();
+        //логинимся курьером и получаем id
+        loginCourierAndGetId();
     }
 
     @Test
     @DisplayName("Тест ручки /api/v1/courier/:id - удаление курьера (успех)")
     @Description("Отправляются существующий id курьера")
     public void deleteCourierTest() {
-        //создаём нового курьера
-        Response response = sendNewCourier();
-
-        //логинимся за ранее созданног курьера и записываем его id
-        getLoginCourier();
-
         //удаляем ранее созданного курьера
-        deleteNewCourier();
+        Response response = deleteNewCourier();
+        //проверка ответа
+        response.then().statusCode(200)
+                .and().body("ok", equalTo(true));
     }
 
     //баг - в тексте сообщения об ошибке "Курьера с таким id нет." - лишняя точка
@@ -41,22 +38,11 @@ public class DeleteCourierTest extends DataHalper {
     @DisplayName("Тест ручки /api/v1/courier/:id - удаление курьера (ошибка 404)")
     @Description("Отправляются несуществующий id курьера")
     public void deleteCourierFalseIdTest() {
-
-        //создаём нового курьера
-        Response response = sendNewCourier();
-
-        //логинимся за ранее созданног курьера и записываем его id
-        getLoginCourier();
-
         //удаление курьера
-        deleteFalseIdNewCourier();
-
-        //почистим базу и удалим созданного курьера
-        given().log().all()
-                .header("Content-type", "application/json")
-                .and()
-                .when()
-                .delete("/api/v1/courier/" + idCourier);
+        Response response = deleteFalseIdNewCourier();
+        //проверка ответа
+        response.then().log().all().statusCode(404)
+                .and().body("message", equalTo("Курьера с таким id нет"));
     }
 
     //баг - в тексте сообщения об ошибке "Not Found.", ошибка 404 (должна быть 400)
@@ -64,85 +50,16 @@ public class DeleteCourierTest extends DataHalper {
     @DisplayName("Тест ручки /api/v1/courier/:id - удаление курьера (ошибка 400)")
     @Description("Не отправляются id курьера")
     public void deleteCourierNoIdTest() {
-
-        //создаём нового курьера
-        Response response = sendNewCourier();
-
-        //логинимся за ранее созданного курьера и записываем его id
-        getLoginCourier();
-
         //удаление курьера
-        deleteNoIdNewCourier();
-
-        //почистим базу и удалим созданного курьера
-        given().log().all()
-                .header("Content-type", "application/json")
-                .and()
-                .when()
-                .delete("/api/v1/courier/" + idCourier);
-    }
-
-
-    @Step("Создаём нового курьера")
-    public Response sendNewCourier() {
-        String json = "{\"login\": \"" + randomLogin + "\", \"password\": \"123\", \"firstName\": \"max1\"}";
-        Response response =
-                given().log().all()
-                        .header("Content-type", "application/json")
-                        .and()
-                        .body(json)
-                        .when()
-                        .post("/api/v1/courier");
-                        response.then().log().all().statusCode(201)
-                        .and().body("ok", equalTo(true));
-        return response;
-    }
-
-    @Step("Логинимся за созданного курьера")
-    public void getLoginCourier() {
-        String json = "{\"login\": \""  + randomLogin + "\", \"password\": \"123\"}";
-        Response response =
-                given().log().all()
-                        .header("Content-type", "application/json")
-                        .and()
-                        .body(json)
-                        .when()
-                        .post("/api/v1/courier/login");
-        JsonPath jsonPathOrder = response.jsonPath();
-        idCourier = jsonPathOrder.get("id");
-    }
-
-    @Step("Удалим ранее созданного курьера")
-    public void deleteNewCourier() {
-        given().log().all()
-                .header("Content-type", "application/json")
-                .and()
-                .when()
-                .delete("/api/v1/courier/" + idCourier)
-                .then().log().all().statusCode(200)
-                .and().body("ok", equalTo(true));
-    }
-
-    @Step("Удалим ранее созданного курьера - не отправляем id курьера")
-    public void deleteNoIdNewCourier() {
-        given().log().all()
-                .header("Content-type", "application/json")
-                .and()
-                .when()
-                .delete("/api/v1/courier/")
-                .then().log().all().statusCode(400)
+        Response response = deleteNoIdNewCourier();
+        //проверка ответа
+        response.then().log().all().statusCode(400)
                 .and().body("message", equalTo("Недостаточно данных для удаления курьера"));
     }
 
-    @Step("Удалим ранее созданного курьера - не отправляем id курьера")
-    public void deleteFalseIdNewCourier() {
-        given().log().all()
-                .header("Content-type", "application/json")
-                .and()
-                .when()
-                .delete("/api/v1/courier/" + "0000")
-                .then().log().all().statusCode(404)
-                .and().body("message", equalTo("Курьера с таким id нет"));
+    @After
+    public void deleteData() {
+        //удаление курьера
+        deleteNewCourier();
     }
-
 }
