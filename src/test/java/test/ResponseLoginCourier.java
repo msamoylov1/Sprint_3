c6271c2be31e85ee5ed6113c594f807382eec00a
Maxim Samoylov <m.samoylov@timepad.ru>
2022-03-05 02:26:52 +0300

    Класс для десериализации ответа при Логине курьера

---
+++ src/test/java/test/ResponseLoginCourier.java
@@ -0,0 +1,15 @@
+package test;
+
+public class ResponseLoginCourier {
+
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+}
