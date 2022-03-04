5dd8136ca6028873e648184dbf620e38662d24d9
Maxim Samoylov <m.samoylov@timepad.ru>
2022-03-05 02:24:09 +0300

    Тесты - Получение списка заказов

--- src/test/java/test/GetListOrdersTest.java
+++ src/test/java/test/GetListOrdersTest.java
@@ -2,31 +2,28 @@ package test;
 
 import io.qameta.allure.Description;
 import io.qameta.allure.junit4.DisplayName;
-import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.Before;
 import org.junit.Test;
 
-import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.notNullValue;
 
-public class GetListOrdersTest {
+public class GetListOrdersTest extends StepsForTests {
 
     @Before
     public void setUp() {
-        RestAssured.baseURI = "http://qa-scooter.praktikum-services.ru";
+        baseUrl();
     }
 
     @Test
     @DisplayName("Проверка ручки /api/v1/orders - получить список заказов")
     @Description("Проверяем верный ли код ответа и содержимое ответа")
     public void getListOrdersTest() {
+        //Получаем список заказов
+        Response response = getListOrders();
 
-        given().log().all()
-                .header("Content-type", "application/json")
-                .when()
-                .get("/api/v1/orders")
-                .then().log().all().statusCode(200)
-                .and().body("orders", notNullValue())
+        //проверяем тело ответа
+        response.then().body("orders", notNullValue())
                 .and().body("orders.id", notNullValue())
                 .and().body("orders.firstName", notNullValue())
                 .and().body("orders.lastName", notNullValue())
