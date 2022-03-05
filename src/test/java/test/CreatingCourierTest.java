5919161a71255e4466e732927c2bdf6432000d2b
Maxim Samoylov <m.samoylov@timepad.ru>
2022-03-05 23:02:23 +0300

    Тесты - Создание курьера

--- src/test/java/test/CreatingCourierTest.java
+++ src/test/java/test/CreatingCourierTest.java
@@ -4,26 +4,18 @@ import io.qameta.allure.Description;
 import io.qameta.allure.junit4.DisplayName;
 import io.restassured.response.Response;
 import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.equalTo;
-import static test.DataHelper.getRandomString;
-
-public class CreatingCourierTest extends StepsForTests {
-
-    @Before
-    public void setUp() {
-        baseUrl();
-        DataHelper.randomLogin = getRandomString(5);
-    }
+public class CreatingCourierTest extends BaseTest {
 
     @Test
     @DisplayName("Тест ручки /api/v1/courier - создания курьера (успех)")
     @Description("Отправляются все 3 поля - корректно заполненные")
     public void createCourierWellCaseTest() {
+        StepsForCreatingCourier stepsForCreatingCourier = new StepsForCreatingCourier();
         //успешно создаём курьера
-        Response response = sendNewCourier();
+        Response response = stepsForCreatingCourier.sendNewCourier();
         //проверяем тело ответа
         response.then().log().all().statusCode(201)
                 .and().body("ok", equalTo(true));
@@ -34,8 +26,9 @@ public class CreatingCourierTest extends StepsForTests {
     @DisplayName("Тест ручки /api/v1/courier - создания курьера дублирование login")
     @Description("Попытка создать курьера с уже использующимся логином - ошибка 409")
     public void createTwoCourierOneLoginTest() {
+        StepsForCreatingCourier stepsForCreatingCourier = new StepsForCreatingCourier();
         //пытаемся создать нового курьера с таким же логином
-        Response response = sendNewDoubleCourier();
+        Response response = stepsForCreatingCourier.sendNewDoubleCourier();
         //проверяем тело ответа
         response.then().log().all().statusCode(409)
                 .and().body("message", equalTo("Этот логин уже используется. Попробуйте другой."));
@@ -45,13 +38,14 @@ public class CreatingCourierTest extends StepsForTests {
     @DisplayName("Тест ручки /api/v1/courier - создания курьера")
     @Description("Отправляются 2 поля(password/firstName)/3 поля, но в login пусто - ошибка 400")
     public void createCourierNoLoginTest() {
+        StepsForCreatingCourier stepsForCreatingCourier = new StepsForCreatingCourier();
         //кейс 1 - пустое поле login
-        Response response = sendNewCourierNullLogin();
+        Response response = stepsForCreatingCourier.sendNewCourierNullLogin();
         //проверка тела ответа
         response.then().log().all().statusCode(400)
                 .and().body("message", equalTo("Недостаточно данных для создания учетной записи"));
         //кейс 2 - пробел в поле login
-        sendNewCourierNoLogin();
+        stepsForCreatingCourier.sendNewCourierNoLogin();
         //проверка тела ответа
         response.then().log().all().statusCode(400)
                 .and().body("message", equalTo("Недостаточно данных для создания учетной записи"));
@@ -61,13 +55,14 @@ public class CreatingCourierTest extends StepsForTests {
     @DisplayName("Тест ручки /api/v1/courier - создания курьера (ошибка 400 - не все данные)")
     @Description("Отправляются 2 поля(login/firstName)/3 поля, но в password пусто - ошибка 400")
     public void createCourierNoPasswordTest() {
+        StepsForCreatingCourier stepsForCreatingCourier = new StepsForCreatingCourier();
         //кейс 1 - пустое поле пароль
-        Response response = sendNewCourierNullPassword();
+        Response response = stepsForCreatingCourier.sendNewCourierNullPassword();
         //проверка тела ответа
         response.then().log().all().statusCode(400)
                 .and().body("message", equalTo("Недостаточно данных для создания учетной записи"));
         //кейс 2 - нет поля пароль
-        sendNewCourierNoPassword();
+        stepsForCreatingCourier.sendNewCourierNoPassword();
         //проверка тела ответа
         response.then().log().all().statusCode(400)
                 .and().body("message", equalTo("Недостаточно данных для создания учетной записи"));
@@ -78,13 +73,14 @@ public class CreatingCourierTest extends StepsForTests {
     @Description("Отправляются 2 поля(login/password)/3 поля, но в firstName пусто - ошибка 400")
     //в этом тесте есть баг - firstName необязательное при создании курьера
     public void createCourierNoFirstNameTest() {
+        StepsForCreatingCourier stepsForCreatingCourier = new StepsForCreatingCourier();
         //кейс 1 - пустое поле фамилия
-        Response response = sendNewCourierNullFirstname();
+        Response response = stepsForCreatingCourier.sendNewCourierNullFirstname();
         //проверка тела ответа
         response.then().log().all().statusCode(400)
                 .and().body("message", equalTo("Недостаточно данных для создания учетной записи"));
         //кейс 2 - нет поля фамилия
-        sendNewCourierNoFirstname();
+        stepsForCreatingCourier.sendNewCourierNoFirstname();
         //проверка тела ответа
         response.then().log().all().statusCode(400)
                 .and().body("message", equalTo("Недостаточно данных для создания учетной записи"));
@@ -92,9 +88,10 @@ public class CreatingCourierTest extends StepsForTests {
 
     @After
     public void deleteData() {
+        StepsForCreatingCourier stepsForCreatingCourier = new StepsForCreatingCourier();
         //получаем id курьера
-        loginCourierAndGetId();
+        stepsForCreatingCourier.loginCourierAndGetId();
         //удаление курьера
-        deleteNewCourier();
+        stepsForCreatingCourier.deleteNewCourier();
     }
 }
