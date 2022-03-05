832afd631b0a29e6878347e3f1b7625df03436a9
Maxim Samoylov <m.samoylov@timepad.ru>
2022-03-05 23:04:30 +0300

    Тесты - Логин курьера

--- src/test/java/test/LoginCourierTest.java
+++ src/test/java/test/LoginCourierTest.java
@@ -11,22 +11,22 @@ import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.Matchers.equalTo;
 import static test.DataHelper.getRandomString;
 
-public class LoginCourierTest extends StepsForTests {
+public class LoginCourierTest extends BaseTest {
 
     @Before
     public void setUp() {
-        baseUrl();
-        DataHelper.randomLogin = getRandomString(5);
+        StepsForLoginCourier StepsForLoginCourier = new StepsForLoginCourier();
         //создаём курьера
-        sendNewCourier();
+        StepsForLoginCourier.sendNewCourier();
     }
 
     @Test
     @DisplayName("Тест ручки /api/v1/courier/login - логин курьера в системе (успех)")
     @Description("Отправляются все 2 поля - корректно заполненные")
     public void trueLoginCourierTest() {
+        StepsForLoginCourier stepsForLoginCourier = new StepsForLoginCourier();
         //логин за ранее созданного курьера
-        Response response = loginCourier();
+        Response response = stepsForLoginCourier.loginCourier();
         //проверка тела ответа
         response.then().log().all().statusCode(200)
                 .and().body("id", notNullValue());
@@ -36,7 +36,8 @@ public class LoginCourierTest extends StepsForTests {
     @DisplayName("Тест ручки /api/v1/courier/login - логин курьера в системе (404 - несуществующий логин)")
     @Description("Отправляются все 2 поля - несуществующий логин, ожидаем ошибку 404")
     public void loginFalseLoginCourierTest() {
-        Response response = getFalseLoginCourier();
+        StepsForLoginCourier stepsForLoginCourier = new StepsForLoginCourier();
+        Response response = stepsForLoginCourier.getFalseLoginCourier();
         //проверка тела ответа
         response.then().log().all().statusCode(404)
                 .and().body("message", equalTo("Учетная запись не найдена"));
@@ -46,7 +47,8 @@ public class LoginCourierTest extends StepsForTests {
     @DisplayName("Тест ручки /api/v1/courier/login - логин курьера в системе (404 - несуществующий пароль)")
     @Description("Отправляются все 2 поля - несуществующий пароль, ожидаем ошибку 404")
     public void loginFalsePassCourierTest() {
-        Response response = getFalsePassCourier();
+        StepsForLoginCourier stepsForLoginCourier = new StepsForLoginCourier();
+        Response response = stepsForLoginCourier.getFalsePassCourier();
         //проверка тела ответа
         response.then().log().all().statusCode(404)
                 .and().body("message", equalTo("Учетная запись не найдена"));
@@ -56,13 +58,14 @@ public class LoginCourierTest extends StepsForTests {
     @DisplayName("Тест ручки /api/v1/courier/login - логин курьера в системе (400 - пустое/пробел в поле логин)")
     @Description("Отправляются 1 поле(password)/2 поля, но в login пусто - ожидаем ошибку 400")
     public void loginNoLoginCourierTest() {
+        StepsForLoginCourier stepsForLoginCourier = new StepsForLoginCourier();
         //кейс 1 - пустое поле логин
-        Response response = getNullLoginCourier();
+        Response response = stepsForLoginCourier.getNullLoginCourier();
         response.then().log().all().statusCode(400)
                 .and().body("message", equalTo("Недостаточно данных для входа"));
 
         //кейс 2 - пробел в поле логин
-        getNoLoginCourier();
+        stepsForLoginCourier.getNoLoginCourier();
         response.then().log().all().statusCode(400)
                 .and().body("message", equalTo("Недостаточно данных для входа"));
     }
@@ -71,19 +74,20 @@ public class LoginCourierTest extends StepsForTests {
     @DisplayName("Тест ручки /api/v1/courier/login - логин курьера в системе (400 - пустое/пробел в поле пароль)")
     @Description("Отправляются 1 поле(login)/2 поля, но в password пусто - ожидаем ошибку 400")
     public void loginNoPassCourierTest() {
+        StepsForLoginCourier stepsForLoginCourier = new StepsForLoginCourier();
         //пробел в поле пароль
-        Response response = getNoPassCourier();
+        Response response = stepsForLoginCourier.getNoPassCourier();
         response.then().log().all().statusCode(400)
                 .and().body("message", equalTo("Недостаточно данных для входа"));
     }
 
     @After
     public void deleteCourier() {
+        StepsForLoginCourier stepsForLoginCourier = new StepsForLoginCourier();
         //получаем id курьера
-        loginCourierAndGetId();
-
+        stepsForLoginCourier.loginCourierAndGetId();
         //удаление курьера
-        deleteNewCourier();
+        stepsForLoginCourier.deleteNewCourier();
 
     }
 }
